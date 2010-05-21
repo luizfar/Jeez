802076691774fc4a9d7f65a7590f2bfbb71cd0c6
Luiz Ribeiro <luizfar@gmail.com>
2010-05-22 02:12:54 +0300

    Interpreter now reads from file

--- src/com/jeez/compiler/Jeez.java
+++ src/com/jeez/compiler/Jeez.java
@@ -1,13 +1,13 @@
 package com.jeez.compiler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Jeez {
 
   
   
-  
-  
-  
-    static final String CODE =
+  static final String CODE2 =
     "class Dog {\n" +
     "  def name\n" +
     "  static def dogCount = 0\n" +
@@ -42,11 +42,26 @@ public class Jeez {
     "println v\n" +
     "// println name.indexOf(\"e\")\n";
   
-  public static void main(String[] args) {
-    new Jeez().run(CODE.toCharArray());
+  public static void main(String[] args) throws Exception {
+    Jeez jeez = new Jeez();
+    
+    String code = jeez.readFile("/home/luiz/j.jz");
+    jeez.run(code.toCharArray());
   }
   
-  public void run(char[] input) {
+  String readFile(String path) throws Exception {
+    BufferedReader reader = new BufferedReader(new FileReader(path));
+    StringBuffer code = new StringBuffer();
+    String line = reader.readLine();
+    while (line != null) {
+      code.append(line + "\n");
+      line = reader.readLine();
+    }
+    
+    return code.toString();
+  }
+  
+  void run(char[] input) {
     new JeezInterpreter().start(input);
   }
 }
